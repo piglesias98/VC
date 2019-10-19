40b85380371c83c0d80510bcc98b94857bf68803
piglesias98 <33418308+piglesias98@users.noreply.github.com>
2019-10-19 21:00:03 +0200

    2c casi terminado
    sale raro pero supresion de maximos está bien

--- pract1.py
+++ pract1.py
@@ -65,12 +65,12 @@ def convolucionGaussiana(imagen, sigma, tam):
 
 def convolucionDerivada(imagen, tam, dx, dy):
     # Filas y columnas de la imagen.
-    height, width, channels = imagen.shape
+    height, width= imagen.shape
     convImg = imagen
     
     #calculamos los kernels
     kernelx, kernely = cv2.getDerivKernels(dx, dy, tam)
-    print(np.dot(kernelx, np.transpose(kernely)))
+    #print(np.dot(kernelx, np.transpose(kernely)))
     #Aplicamos la convolución
     convImg[:]=cv2.filter2D(imagen[:],-1,kernelx.T)
     convImg[:,:width]=cv2.filter2D(imagen[:,:width],-1,kernely)
@@ -166,7 +166,7 @@ def pirLaplaciana(imagen, niveles):
 
 #PRUEBA EJERCICIOS
 
-rutaImagen = 'C:\\Users\\Paula\\Documents\\VC\\practica1\\images\\marilyn.bmp'
+rutaImagen = 'C:\\Users\\Paula\\Documents\\VC\\practica1\\images\\cat.bmp'
 #imagen = leeImagen(rutaImagen, False)
 imagen = cv2.imread(rutaImagen, 0)
 #print(imagen)
@@ -248,16 +248,16 @@ def display_piramide(img,color=True):
     
     
 
-def display_nuevo(img):
-    for im in img:
-        imgt = (np.clip(im,0,1)*255.).astype(np.uint8)
-        height, width = imgt.shape
-        figsize = width, height
-        plt.figure(figsize=figsize)
-        plt.imshow(imgt)
-        plt .close()
-    plt.show()
-    
+#def display_nuevo(img):
+#    for im in img:
+#        imgt = (np.clip(im,0,1)*255.).astype(np.uint8)
+#        height, width = imgt.shape
+#        figsize = width, height
+#        plt.figure(figsize=figsize)
+#        plt.imshow(imgt)
+#        plt .close()
+#    plt.show()
+#    
     
 #display_piramide(piramide)
 #for i in range(len(piramide)):
@@ -304,10 +304,10 @@ def representar_imagenes(lista_imagen_leida, lista_titulos):
     
     
 #
-piramide = piramideLaplaciana(imagen, 10)
-print("AHORA ESTAMOS EN PIRAMIDE")
-titulos = ["1","2","3","4","5","6","7","8","9","10", "11"]
-representar_imagenes(piramide, titulos)
+#piramide = piramideLaplaciana(imagen, 10)
+#print("AHORA ESTAMOS EN PIRAMIDE")
+#titulos = ["1","2","3","4","5","6","7","8","9","10", "11"]
+#representar_imagenes(piramide, titulos)
 
 #
 #x = np.array([[0,1,2,3,4],
@@ -336,7 +336,7 @@ representar_imagenes(piramide, titulos)
 #plt.imshow(pl[10], cmap="gray")
 #    
     
-kernel = np.array([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[17,18,19,20,21],[22,23,25,26,27]])   
+#kernel = np.array([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[17,18,19,20,21],[22,23,25,26,27]])   
 #print(kernel)
 #height, width = kernel.shape
 #print("concantemanos")
@@ -365,3 +365,120 @@ kernel = np.array([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[17,18,19,20,21],[2
 #ap=ap.T
 #print(ap)
 #print(sobremuestrearDuplicar(kernel))
+    
+    
+    
+    
+def busquedaRegiones(imagen, escalas, sigma):
+    for i in range(escalas):
+        img = convolucionGaussiana(imagen, sigma, -1)
+        img = convolucionLaplaciana(imagen, sigma)
+        nImg=img
+        nImg = cv2.normalize(img,  nImg, 0, 255, cv2.NORM_MINMAX)
+        #Aplicamos el cuadrado
+        cImg=nImg
+        cImg = np.square(nImg)
+    #Realizamos la supresión de no máximos
+    sImg = cImg
+    sImg= supresionNoMaximos(cImg)
+    #Mostrar las regiones encontradas con sus correspondientes escalas
+    return circulos(sImg, sigma)
+
+
+def supresionNoMaximos(imagen):
+    height, width = imagen.shape[:2]
+    nueva= np.copy(imagen)
+    #Recorremos cada pixel
+    for i in range(height):
+        for j in range(width):
+            nueva[i-1:i+2, j-1:j+2] = np.where(imagen[i-1:i+2, j-1:j+2] > imagen[i,j], 0, nueva[i-1:i+2, j-1:j+2])
+    return nueva
+
+def circulos(imagen,sigma):
+    height, width = imagen.shape[:2]
+    #Recorremos cada pixel
+    for i in range(height):
+        for j in range(width):
+            if imagen[i,j]>124:
+                radio = int(math.sqrt(2) * sigma)
+                color = (255, 0, 0)
+                cv2.circle(imagen, (i,j), radio, color)
+    return imagen
+                
+                
+img = busquedaRegiones(imagen, 3, 3)
+plt.imshow(img, cmap="gray")
+
+#Comprobamos los inicios y los finales
+#            if (i-1)>=0:    inicio_i = i-1
+#            else:   inicio_i = 0
+#            
+#            if (j-1)>=0:    inicio_j = j-1
+#            else:   inicio_j = 0
+#            
+#            if (i+1)<height: final_i=i+1
+#            else:   final_i=height-1
+#            
+#            if (j+1)<width: final_j=j+1
+#            else:   final_j=width-1
+            #Recorremos los adyacentes
+#            nueva[inicio_i:final_i+1,inicio_j:final_j+1] = np.where(imagen[inicio_i:final_i+1,inicio_j:final_j+1] > imagen[i,j],0,nueva[inicio_i:final_i+1,inicio_j:final_j+1])         
+
+
+#
+#kernel = np.array([[22,23,25,26,27],[17,18,19,20,21],[11,12,13,14,15],[17,18,19,20,21],[22,23,25,26,27]]) 
+#height, width = kernel.shape[:2]  
+#print(kernel)
+##print(supresionNoMaximos(kernel))
+#nueva=np.copy(kernel)
+#for i in range(height):
+#    for j in range(width):
+#        print(i)
+#        print(j)
+#        pixel = kernel[i,j]
+#        print(kernel[i,j])
+#        #Recorremos los adyacentes
+#        if (i-1)>=0:    inicio_i = i-1
+#        else:   inicio_i = 0
+#        
+#        if (j-1)>=0:    inicio_j = j-1
+#        else:   inicio_j = 0
+#        
+#        if (i+1)<height: final_i=i+1
+#        else:   final_i=height-1
+#        
+#        if (j+1)<width: final_j=j+1
+#        else:   final_j=width-1
+#        
+#        print("kernel")
+#        print(kernel[inicio_i:final_i+1,inicio_j:final_j+1])
+##        submatriz = np.where(kernel[inicio_i:final_i+1,inicio_j:final_j+1] > kernel[i,j], 0, kernel[inicio_i:final_i+1,inicio_j:final_j+1])
+##        print("submatriz")
+##        print(submatriz)
+#        nueva[inicio_i:final_i+1,inicio_j:final_j+1] = np.where(kernel[inicio_i:final_i+1,inicio_j:final_j+1] > kernel[i,j],0,nueva[inicio_i:final_i+1,inicio_j:final_j+1])
+#        print("nueva")
+#        print(nueva)
+#               
+#    
+##        
+#        
+#i= 0
+#j=0
+#print(kernel)
+#print(i)
+#print(j)
+#print(kernel[i,j])
+#if (i-1)>=0:    inicio_i = i-1
+#else:   inicio_i = 0
+#
+#if (j-1)>=0:    inicio_j = j-1
+#else:   inicio_j = 0
+#
+#if (i+1)<height: final_i=i+1
+#else:   final_i=height-1
+#
+#if (j+1)<width: final_j=j+1
+#else:   final_j=width-1
+#print("final")
+#print(final_i) 
+#print(kernel[inicio_i:final_i+1,inicio_j:final_j+1])        
