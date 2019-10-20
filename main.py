2c6033cf82847706e4970f414b77b2e39674129d
piglesias98 <33418308+piglesias98@users.noreply.github.com>
2019-10-20 22:57:51 +0200

    ejercicio 1b en proceso

--- main.py
+++ main.py
@@ -47,37 +47,72 @@ def representarImagenes(lista_imagen_leida, lista_titulos, n_col):
 
 	plt.show()
 
-def convolucionGaussiana(imagen, sigma):
+def gaussiana(imagen, sigma):
     img = np.copy(imagen)
     tam = 6*sigma +1
     kernel = cv2.getGaussianKernel(tam, sigma)
-    imgConv = convolucionSeparada(img, kernel)
+    imgConv = convolucion(img, kernel, kernel)
     return imgConv
 
-def convolucionSeparada(imagen, kernel):
-    img = np.copy(imagen)
-    # Filas y columnas de la imagen.
-    height, width = img.shape[:2]
-    convImg = img
-    # Recorremos filas y columnas.
-    convImg[0:height]=cv2.filter2D(img[:],-1,kernel)
-    convImg[:,0:width]=cv2.filter2D(img[:,0:width],-1,kernel.T)
-
-    return convImg
-
-def convolucionDerivada(imagen, tam, dx, dy):
-    img = np.copy(imagen)
-    # Filas y columnas de la imagen.
-    height, width= img.shape
-    convImg = img
+#def convolucionSeparada(imagen, kernel):
+#    img = np.copy(imagen)
+#    # Filas y columnas de la imagen.
+#    height, width = img.shape[:2]
+#    convImg = img
+#    # Recorremos filas y columnas.
+#    convImg[0:height]=cv2.filter2D(img[:],-1,kernel)
+#    convImg[:,0:width]=cv2.filter2D(img[:,0:width],-1,kernel.T)
+#
+#    return convImg
     
+def convolucion(imagen, kernelx, kernely, border=cv2.BORDER_DEFAULT, normalize=True):
+    kernelx = np.flip(kernelx)
+    kernely= np.flip(kernely)
+    img = np.copy(imagen)
+    height, width = imagen.shape[:]
+    for i in range(height):
+        resConv = cv2.filter2D(img[i, :], -1, kernelx, borderType=border)
+        img[i, :] = [sublist[0] for sublist in resConv]
+    for j in range(width):
+        resConv = cv2.filter2D(img[:, j], -1, kernely, borderType=border)
+        img[:, j] = [sublist[0] for sublist in resConv]
+    if normalize:
+        cv2.normalize(img, img, 0, 255, cv2.NORM_MINMAX)
+    return img
+
+#def convolucionDerivada(imagen, tam, dx, dy):
+#    img = np.copy(imagen)
+#    # Filas y columnas de la imagen.
+#    height, width= img.shape
+#    convImg = img
+#    
+#    #calculamos los kernels
+#    kernelx, kernely = cv2.getDerivKernels(dx, dy, tam)
+#    #print(np.dot(kernelx, np.transpose(kernely)))
+#    #Aplicamos la convolución
+#    convImg[:]=cv2.filter2D(img[:],-1,kernelx.T)
+#    convImg[:,:width]=cv2.filter2D(img[:,:width],-1,kernely)
+#    return convImg
+    
+def derivada(imagen, tam, dx, dy):
+    # Filas y columnas de la imagen.
+    height, width= imagen.shape[:2]
+    img = np.copy(imagen)
     #calculamos los kernels
-    kernelx, kernely = cv2.getDerivKernels(dx, dy, tam)
-    #print(np.dot(kernelx, np.transpose(kernely)))
-    #Aplicamos la convolución
-    convImg[:]=cv2.filter2D(img[:],-1,kernelx.T)
-    convImg[:,:width]=cv2.filter2D(img[:,:width],-1,kernely)
+    kernelx, kernely = cv2.getDerivKernels(dx,dy,tam)
+    #Realizamos la convolucion
+    convImg=convolucion(img, kernelx, kernely)
     return convImg
+
+def laplaciana(imagen, tam, borde=cv2.BORDER_DEFAULT):
+    img = np.copy(imagen)
+    cv2.getDerivKernels(2, 0, tam)
+    kernelx1, kernelx2 = cv2.getDerivKernels(2, 0, tam)
+    kernely1, kernely2 = cv2.getDerivKernels(0, 2, tam)
+    img_x = convolucion(img, kernelx1, kernelx2, borde)
+    img_y = convolucion(img, kernely1, kernely2, borde)
+    return img_x+img_y
+
 
 
 #Leer imágenes
@@ -99,10 +134,10 @@ bicicleta = cv2.imread('C:\\Users\\Paula\\Documents\\VC\\practica1\\images\\bicy
 def ejercicio1a():
     print("Ejercicio 1 - Apartado A")
     print("Máscara gaussiana")
-    sigma3 = convolucionGaussiana(pajaro, 3)
+    sigma3 = gaussiana(pajaro, 3)
     imagenes = [sigma3]
     titulos = ["Sigma = 3"]
-    sigma5= convolucionGaussiana(pajaro, 5)
+    sigma5= gaussiana(pajaro, 5)
     imagenes.append(sigma5)
     titulos.append("Sigma = 5")
     representarImagenes(imagenes, titulos, 2)
@@ -113,13 +148,19 @@ def ejercicio1a():
         derivadas.append(convolucionDerivada(gato, i, 1, 0))
         tit = "sigma = "+str(i)+" Dx=1, Dy=0"
         titulos.append(tit)
-        derivadas.append(convolucionDerivada(gato, i, 0, 1))
+        derivadas.append(derivada(gato, i, 0, 1))
         tit = "sigma = "+str(i)+" Dx=0, Dy=1"
         titulos.append(tit)
-        derivadas.append(convolucionDerivada(gato, i, 1, 1))
+        derivadas.append(derivada(gato, i, 1, 1))
         tit = "sigma = "+str(i)+" Dx=1, Dy=1"
         titulos.append(tit)
     representarImagenes(derivadas, titulos, 3)
+
+def ejercicio1b():
+    print("Ejercicio 1 - Apartado A")
+    print("Máscara laplaciana")
+    
+
 
 ejercicio1a()
         
